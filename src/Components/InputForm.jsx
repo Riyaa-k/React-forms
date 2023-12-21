0c1c47c9d75964f80900443e614fd176fd8607df
Riyaa-k <anshitakoshta28@gmail.com>
2023-12-21 23:44:50 +0530

    Done

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const InputForm = () => {
   const initialValues = {
@@ -25,6 +25,13 @@ const InputForm = () => {
     setFormErrors(validate(formValues));
     setIsSubmit(true);
   }
+  useEffect(() => {
+    console.log(formErrors)
+    if(Object.keys(formErrors).length===0 && isSubmit){
+        console.log(formValues)
+    }
+  }, [formErrors])
+  
 
   const validate =(values)=>{
     const errors={};
@@ -46,22 +53,28 @@ const InputForm = () => {
   return (
     <>
       <div className="Form">
+        {Object.keys(formErrors).length===0 && isSubmit ? (<div>Submitted</div>):<pre>
+            {JSON.stringify(formValues,undefined,2)}</pre>}
         <form onSubmit={handleSubmit} >
             <label htmlFor="first">First Name</label>
             <input type="text" name="first" value={formValues.first} onChange={handleChange}/>
             <br />
+            <p>{formErrors.first}</p>
 
             <label htmlFor="last">Last Name</label>
             <input type="text" name="last" value={formValues.last} onChange={handleChange}/>
             <br />
+            <p>{formErrors.last}</p>
 
             <label htmlFor="email">Email</label>
             <input type="text" name="email" value={formValues.email} onChange={handleChange}/>
             <br />
+            <p>{formErrors.email}</p>
 
             <label htmlFor="contacts">Contacts</label>
             <input type="text" name="contacts" value={formValues.contacts} onChange={handleChange}/>
             <br />
+            <p>{formErrors.contacts}</p>
 
             <button className="submit">Submit</button>
         </form>
